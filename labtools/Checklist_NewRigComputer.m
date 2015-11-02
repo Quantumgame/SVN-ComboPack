45b74a1be8de32abd0ae88eab857639c27ce3118
wehrlab <wehrlab@cd1d7de1-a1fc-8c46-85bb-22c391850971>
2015-11-02 19:04:45 +0000

    git-svn-id: svn://blister.uoregon.edu/exper/trunk@1038 cd1d7de1-a1fc-8c46-85bb-22c391850971

---
+++ labtools/Checklist_NewRigComputer.m
@@ -0,0 +1,11 @@
+checklist for setting up a new rig computer
+install svn and tortoise - see SVNWehrlab.pdf
+check out exper
+install nidaqmx drivers: http://www.ni.com/downloads/ni-drivers/
+download and install matlab 32 for exper and 64 also if you want it for processing
+right click matlab shortcut, select properties/compatibility, check "run as administrator"
+install psychtoolbox
+install Lynx drivers http://www.lynxstudio.com/support_downloads_results.asp
+install deltacopy client and configure backups to blister.uoregon.edu. http://www.aboutmyip.com/AboutMyXApp/DeltaCopy.jsp
+    Make sure to uncheck "delete files from server" !
+if you get a api-ms-win-crt-runtime-l1-1-0.dll missing error, re-install Visual C++ Redistributable for Visual Studio 2015 (64-bit)
