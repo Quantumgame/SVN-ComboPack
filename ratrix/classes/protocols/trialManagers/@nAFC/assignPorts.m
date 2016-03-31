f71a6d4628865e7a7010b2ea92808d6754150651
wehr <wehr@cd1d7de1-a1fc-8c46-85bb-22c391850971>
2016-03-31 06:35:30 +0000

    Merging Recent SpeechDiscrim Changes
    git-svn-id: svn://blister.uoregon.edu/exper/trunk@1078 cd1d7de1-a1fc-8c46-85bb-22c391850971

--- ratrix/classes/protocols/trialManagers/@nAFC/assignPorts.m
+++ ratrix/classes/protocols/trialManagers/@nAFC/assignPorts.m
@@ -2,6 +2,7 @@ function [targetPorts, distractorPorts, details, text] = assignPorts(trialManage
 lastResult = [];
 lastCorrect = [];
 lastWasCorrection = false;
+tooBiased = 0;
 
 if ~isempty(trialRecords) && length(trialRecords)>1
     lastRec=trialRecords(end-1);
@@ -21,6 +22,37 @@ if ~isempty(trialRecords) && length(trialRecords)>1
         lastWasCorrection = lastRec.stimDetails.correctionTrial;
     end
     
+    try % check for bias
+        numtrials = length(trialRecords);
+        lefts = [];
+        rights = [];
+        j = 1;
+        if numtrials>52
+            for i = numtrials-51:numtrials-1
+                lefts(j) = trialRecords(i).phaseRecords(2).responseDetails.tries{1}(1);
+                rights(j) = trialRecords(i).phaseRecords(2).responseDetails.tries{1}(3);
+                j = j+1;
+            end
+            leftpct = mean(lefts);
+            rightpct = mean(rights);
+            biaspct = leftpct-rightpct;
+            if biaspct < (-.3)
+                tooBiased = 1;
+                unBiasedPort = 1;
+                biasedPort = 3;
+            elseif biaspct > (.3)
+                tooBiased = 1;
+                unBiasedPort = 3;
+                biasedPort = 1;
+            else
+                tooBiased = 0;
+            end
+        end
+
+    end
+            
+        
+    
     if length(lastResult)>1
         lastResult = lastResult(1);
     end
@@ -31,15 +63,26 @@ if ~isempty(lastCorrect) && ...
         ~isempty(lastResult) && ...
         ~lastCorrect && ...
         (length(lastRec.targetPorts)==1 || strcmp(trialRecords(end).trialManagerClass,'ball')) && ... %ugh!
-        (lastWasCorrection || rand<trialManager.percentCorrectionTrials)
+        (lastWasCorrection || rand<trialManager.percentCorrectionTrials || tooBiased) %Changing so correction trials calculated by sliding window instead of just last trial -JLS030916
     
     details.correctionTrial = 1;
     try
     details.startTone=lastRec.stimDetails.startTone;
     details.endTone=lastRec.stimDetails.endTone;
     end
-    targetPorts = lastRec.targetPorts;
-    text = 'correction trial!';
+    if tooBiased 
+         if rand<(.75)
+            targetPorts = unBiasedPort;
+            text = 'Bias correction trial!';
+        else
+            targetPorts = biasedPort;
+            text = 'Reverse Bias correction trial!';
+        end
+    else
+        targetPorts = lastRec.targetPorts;
+        text = 'Regular correction trial!';
+    end
+    
 else
     details.correctionTrial = 0;
     targetPorts = responsePorts(ceil(rand*length(responsePorts)));
