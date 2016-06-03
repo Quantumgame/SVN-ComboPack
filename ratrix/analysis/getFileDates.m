af38badde41cb3ae5bd45bcd28a92d7f0a99c9a2
wehr <wehr@cd1d7de1-a1fc-8c46-85bb-22c391850971>
2016-06-03 01:24:44 +0000

    Data cleaning functions pretty much complete
    git-svn-id: svn://blister.uoregon.edu/exper/trunk@1134 cd1d7de1-a1fc-8c46-85bb-22c391850971

--- ratrix/analysis/getFileDates.m
+++ ratrix/analysis/getFileDates.m
@@ -1,19 +1,15 @@
-function fileList = getFilenames(dirName)
+function dateList = getFileDates(dirName)
   excludes = {'.DS_Store','.','..'};
   dirData = dir(dirName);      %# Get the data for the current directory
   dirIndex = [dirData.isdir];  %# Find the index for directories
-  fileList = {dirData(~dirIndex).name}';  %'# Get a list of the files
-  validFiles = ~ismember(fileList,excludes);
-  fileList = fileList(validFiles);
-  if ~isempty(fileList)
-    fileList = cellfun(@(x) fullfile(dirName,x),...  %# Prepend path to files
-                       fileList,'UniformOutput',false);
-  end
+  dateList = {dirData(~dirIndex).date}';  %'# Get a list of the files
+  validFiles = ~ismember(dateList,excludes);
+  dateList = dateList(validFiles);
   subDirs = {dirData(dirIndex).name};  %# Get a list of the subdirectories
   validIndex = ~ismember(subDirs,{'.','..','Unprocessed','Unsorted'});  %# Find index of subdirectories
                                                %#   that are not '.' or '..'
   for iDir = find(validIndex)                  %# Loop over valid subdirectories
     nextDir = fullfile(dirName,subDirs{iDir});    %# Get the subdirectory path
-    fileList = [fileList; getFilenames(nextDir)];  %# Recursively call getAllFiles
+    dateList = [dateList; getFileDates(nextDir)];  %# Recursively call getAllFiles
   end
 end
