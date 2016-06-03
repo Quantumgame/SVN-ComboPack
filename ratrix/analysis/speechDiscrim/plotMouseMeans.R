5d98ee7d3ab8e0dbd27fa65ca1a885308950380f
wehr <wehr@cd1d7de1-a1fc-8c46-85bb-22c391850971>
2016-06-03 01:25:09 +0000

    Maybe we have escaped the storm
    git-svn-id: svn://blister.uoregon.edu/exper/trunk@1137 cd1d7de1-a1fc-8c46-85bb-22c391850971

--- ratrix/analysis/speechDiscrim/plotMouseMeans.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-library(reshape)
-
-sm <- read.csv('~/Dropbox/Lab Self/inFormants/Behavior Data/sessionMeans.csv')
-sm$numtrials = seq(1,nrow(sm),1)
-
-smb <- melt(sm,id.vars = )
-ggplot(data=sm, aes(x=))
